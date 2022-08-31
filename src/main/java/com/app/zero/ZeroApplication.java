3536ea7bf10b8df7edb31340bccd1e7e02f73138
yejin <yaezzin@gmail.com>
2022-09-01 03:39:40 +0900

    initial commit

---
+++ src/main/java/com/app/zero/ZeroApplication.java
@@ -0,0 +1,13 @@
+package com.app.zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroApplication.class, args);
+	}
+
+}
