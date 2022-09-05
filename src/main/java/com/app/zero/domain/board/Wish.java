45be26a7608f754e33154e52ae15030ed72a8c39
yejin <yaezzin@gmail.com>
2022-09-05 15:32:03 +0900

    feat : wish entity

---
+++ src/main/java/com/app/zero/domain/board/Wish.java
@@ -0,0 +1,26 @@
+package com.app.zero.domain.board;
+
+import com.app.zero.domain.user.User;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Board board;
+
+    @ManyToOne
+    private User user;
+
+    public Wish(Board board, User user) {
+        this.board = board;
+        this.user = user;
+    }
+}
